5e1d3ae40ecb5054b4f15a93a8fb0795e629eb5e
adamprinciotta <adamprinciotta@gmail.com>
2023-04-27 16:16:29 -0400

    The first section of the home page is done

--- portofino-tile/src/App.jsx
+++ portofino-tile/src/App.jsx
@@ -7,36 +7,44 @@ import Reviews from "./components/Reviews.jsx";
 import BeforeAfter from "./components/BeforeAfter.jsx";
 import Contact from "./components/Contact.jsx"
 
+import logo from "./images/portofino-tile-logo.png"
+
 function App() {
   return (
     <div className="container">
-      
       <Router>
-        <nav className="navLinks">
-          {/* <a href="tel:+7812210334" className="tapToCall"></a> */}
+        <div className="header">
           <div className="imageContainer">
+            <img src={logo} width="282px" height="142px"></img>
           </div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/About">About Us</Link>
-            </li>
-            <li>
-              <Link to="/BathRemodel">Bath Remodeling</Link>
-            </li>
-            <li>
-              <Link to="/Reviews">Reviews</Link>
-            </li>
-            <li>
-              <Link to="/BeforeAndAfter">Before & After</Link>
-            </li>
-            <li>
-              <Link to="/Contact">Contact Us</Link>
-            </li>
-          </ul>
-        </nav>
+            <nav className="navLinks">
+              {/* <a href="tel:+7812210334" className="tapToCall"></a> */}
+              
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/About">About Us</Link>
+                </li>
+                <li>
+                  <Link to="/BathRemodel">Bath Remodeling</Link>
+                </li>
+                <li>
+                  <Link to="/Reviews">Reviews</Link>
+                </li>
+                <li>
+                  <Link to="/BeforeAndAfter">Before & After</Link>
+                </li>
+                <li>
+                  <Link to="/Contact">Contact Us</Link>
+                </li>
+              </ul>
+              <div className="call">
+                <h4>Call Us Now <Link to="/Contact">919-298-2257</Link></h4>
+              </div>
+            </nav>
+          </div>
         <div className="routeContainer">
           <Routes>
             <Route exact path="/" element={<Home />} />
--- portofino-tile/src/components/Home.jsx
+++ portofino-tile/src/components/Home.jsx
@@ -1,8 +1,55 @@
-import {react} from 'react';
+import {react, useEffect, useState} from 'react';
+import homepageVideo from '../images/homepage-fade.mp4'
+import homeCenter from '../images/HomeCenter.png'
+import '../styles/Home.css'
 
 function Home(){
+
+    const [current, setCurrent] = useState(0);
+    const messages = [
+      "Complete One-Stop-Shop showroom & Design Center",
+      "Luxury For Less...Designed For Your Life...",
+      "The Triangle's Only Complete Contractor & Design Center",
+      "Expert Design and Installation Average 9-11 days!",
+    ];
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        const next = ((current + 1) % 4)
+        console.log("next: ", next)
+        // current === messages.length - 1 ? 0 : current + 1;
+
+        setCurrent(next);
+      }, 4000);
+  
+      return () => clearInterval(interval);
+    }, [current, messages.length]);
+
+
     return(
-        <h1>Home</h1>
+        <>
+        <div className="homeContainer">
+            <video src={homepageVideo} autoPlay="autoplay" preload="auto" loop="loop" muted="muted"></video>
+            <section>
+            <div className="homeCenterDiv">
+                <img src={homeCenter} width="50%"></img>
+                <br></br>
+                {messages.map((message, index) => (
+                <h2 key={index} className={index === current ? "current" : ""}>
+                    {message}
+                </h2>
+            ))}
+            <br></br>
+            <button className='homeBtn'>
+                <div className="homeBtnTop">How do I get started?</div>
+                <div className="homeBtnBottom">Click here or scroll down</div>
+            </button>
+            <br></br>
+            <h1>NC LICENSED GENERAL CONTRACTOR #86537</h1>
+            </div>
+            </section>
+        </div>
+        </>
     )
 }
 
